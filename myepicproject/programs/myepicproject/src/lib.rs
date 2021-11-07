d8ffe529fe32504f9032b276f0d493651e36111e
Wbert Adrian Castro Vera <wcastrov@perficient.com>
2021-11-06 19:15:48 -0500

    Initial commit

---
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -0,0 +1,61 @@
+use anchor_lang::prelude::*;
+
+declare_id!("7PRJHwrQBVyNwuK2bfAf7fponi73Q7bmGSGsjvU5twoJ");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *base_account.to_account_info().key,
+            votes: 0,
+        };
+
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer=user, space=9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(Accounts)]
+pub struct VoteGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Debug, Clone)]
+pub struct ItemStruct  {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub votes: u32,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
+}
---
+++ myepicproject/tests/myepicproject.js
@@ -0,0 +1,54 @@
+const anchor = require('@project-serum/anchor');
+
+const {SystemProgram} = anchor.web3;
+
+const main = async () => {
+    console.log('Starting test...');
+
+    const provider = anchor.Provider.env();
+    anchor.setProvider(provider);
+  
+    const program = anchor.workspace.Myepicproject;
+	
+	// Create an account keypair for our program to use.
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    const tx  = await program.rpc.startStuffOff(
+        {
+            accounts: {
+                baseAccount: baseAccount.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+            },
+            signers: [
+                baseAccount
+            ]
+        }
+    );
+
+    console.log('Transaction Signature:', tx);
+
+    let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+    console.log('Kitty Gif Account', account.totalGifs.toString());
+
+    await program.rpc.addGif('https://c.tenor.com/i39K1COMe3oAAAAM/kitten-falls-kitten-stumbling.gif', {
+        accounts: {
+            baseAccount: baseAccount.publicKey,
+        }
+    });
+
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+    console.log('new Kitty Gif Account', account.totalGifs.toString());
+
+    console.log('Gif list: ', account.gifList);
+};
+
+main().then(() => {
+    console.log('Test complete');
+    process.exit(0);
+}).catch(e => {
+    console.error(e);
+    process.exit(1);
+});
